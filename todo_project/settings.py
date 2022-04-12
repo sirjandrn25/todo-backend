a26f02d0be3ce9a518806809af48d3e05c630dc5
sirjan tamang <sirjantmg99@gmail.com>
2022-04-12 23:10:34 +0545

    add static

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -134,12 +134,19 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+if DEBUG:
+    # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "task.User"
+
 
 # REST_FRAMEWORK = {
 #     'DEFAULT_AUTHENTICATION_CLASSES': (
