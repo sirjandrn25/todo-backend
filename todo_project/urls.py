bbfe40a2b1d7b88384d3115d01213840895bea4f
sirjan tamang <sirjantmg99@gmail.com>
2022-04-14 18:54:18 +0545

    serve and settings add

--- todo_project/urls.py
+++ todo_project/urls.py
@@ -18,6 +18,9 @@ from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.views.static import serve
+from django.conf import settings
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Snippets API",
