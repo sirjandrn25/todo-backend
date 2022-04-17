840e4656d8226148c38be9439594bba8c8424d16
sirjan tamang <sirjantmg99@gmail.com>
2022-04-17 19:03:48 +0545

    permission fix

--- task/permissions.py
+++ task/permissions.py
@@ -7,9 +7,5 @@ from rest_framework import permissions
 
 class IsUserTaskPermission(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method == 'DELETE':
-            return request.user.is_superuser
-        else:
-            if request.method in permissions.SAFE_METHODS or request.user.is_superuser:
-                return True
-            return request.user == obj
+        
+        return request.user == obj.user
